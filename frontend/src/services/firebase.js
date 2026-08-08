4aa74550d0956b2cb663365c28738165b77e9a6c
QuantumByte_14 <srushtigelani@gmail.com>
2026-08-08 19:55:05 +0530

    Move Firebase API key to environment variable

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "REDACTED_FIREBASE_API_KEY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "careerfit-a83cf.firebaseapp.com",
   projectId: "careerfit-a83cf",
   storageBucket: "careerfit-a83cf.firebasestorage.app",
